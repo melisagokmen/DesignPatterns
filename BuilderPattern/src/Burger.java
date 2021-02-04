63fffdcf7ac3c53cacf127f859b3056f4fa78e68
melisagokmen <52598282+melisagokmen@users.noreply.github.com>
2021-02-04 14:42:25 +0300

    Builder pattern has been added

---
+++ BuilderPattern/src/Burger.java
@@ -0,0 +1,6 @@
+public abstract class Burger implements Item{
+    public Packing packing(){
+        return new Wrapper();
+    }
+    public abstract float price();
+}
---
+++ BuilderPattern/src/Pepsi.java
@@ -0,0 +1,14 @@
+
+public class Pepsi extends ColdDrink {
+
+    @Override
+    public float price() {
+        return 35.5f;
+    }
+
+    @Override
+    public String name() {
+        return "Pepsi";
+    }
+
+}
