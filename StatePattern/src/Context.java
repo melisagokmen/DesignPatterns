e499f764e3f92494f6e03fd130c9a33a879b7c10
melisagokmen <52598282+melisagokmen@users.noreply.github.com>
2021-05-01 23:59:27 +0300

    State pattern has been added.

---
+++ StatePattern/src/Context.java
@@ -0,0 +1,18 @@
+
+public class Context {
+
+    private State state;
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public Context() {
+        state = null;
+    }
+
+}
